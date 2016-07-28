ea20f896b9e51aa95a83cca3aab573c10f8a65bc
Víðir Valberg Guðmundsson <valberg@orn.li>
2016-07-28 14:15:12 +0200

    Adding initial model structure

--- jeopardy/admin.py
+++ jeopardy/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Answer)
+class AnswerModelAdmin(admin.ModelAdmin):
+    pass
+
+
+class AnswerInlineAdmin(admin.TabularInline):
+    model = models.Answer
+
+
+@admin.register(models.Category)
+class CategoryModelAdmin(admin.ModelAdmin):
+    inlines = [AnswerInlineAdmin]
+
+
+@admin.register(models.Round)
+class RoundModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Game)
+class GameModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Player)
+class PlayerModelAdmin(admin.ModelAdmin):
+    pass
+
--- jeopardy/models.py
+++ jeopardy/models.py
@@ -1,3 +1,109 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class OrderWithRespectToConstraintMixin(models.Model):
+
+    child_limit = None
+
+    class Meta:
+        abstract = True
+
+    def clean(self):
+        super().clean()
+
+        key = self._meta.order_with_respect_to.attname
+
+        kwargs = {
+            key: getattr(self, key)
+        }
+
+        if self.__class__.objects.filter(**kwargs).count() >= self.child_limit:
+            raise ValidationError('Limit of {} reached'.format(self.child_limit))
+
+
+class Answer(OrderWithRespectToConstraintMixin, models.Model):
+    class Meta:
+        verbose_name = 'Answer'
+        verbose_name_plural = 'Answers'
+        order_with_respect_to = 'category'
+
+    answer = models.CharField(max_length=255)
+    category = models.ForeignKey('jeopardy.Category')
+
+    child_limit = 5
+
+    VIDEO = 'video'
+    IMAGE = 'image'
+    AUDIO = 'audio'
+    CHANCE = 'chance'
+    TEXT = 'text'
+    ANSWER_TYPES = [
+        (VIDEO, 'Video'),
+        (IMAGE, 'Image'),
+        (AUDIO, 'Audio'),
+        (CHANCE, 'Chance'),
+        (TEXT, 'Text'),
+    ]
+
+    answer_type = models.CharField(
+        max_length=10,
+        choices=ANSWER_TYPES,
+        default=TEXT
+    )
+
+    def __str__(self):
+        return self.answer
+
+
+class Category(OrderWithRespectToConstraintMixin, models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        order_with_respect_to = 'round'
+
+    child_limit = 6
+
+    title = models.CharField(max_length=255)
+    round = models.ForeignKey('jeopardy.Round')
+
+    def __str__(self):
+        return self.title
+
+
+class Round(OrderWithRespectToConstraintMixin, models.Model):
+    class Meta:
+        verbose_name = 'Round'
+        verbose_name_plural = 'Rounds'
+        order_with_respect_to = 'game'
+
+    child_limit = 3
+
+    game = models.ForeignKey('jeopardy.Game')
+
+
+class Game(models.Model):
+    class Meta:
+        verbose_name = 'Game'
+        verbose_name_plural = 'Games'
+
+    title = models.CharField(max_length=255)
+
+    player1 = models.ForeignKey('jeopardy.player', related_name='player1')
+    player2 = models.ForeignKey('jeopardy.player', related_name='player2')
+    player3 = models.ForeignKey('jeopardy.player', related_name='player3')
+
+    def __str__(self):
+        return self.title
+
+
+class Player(models.Model):
+    class Meta:
+        verbose_name = 'Player'
+        verbose_name_plural = 'Players'
+
+    name = models.CharField(max_length=255)
+    points = models.IntegerField()
+
+    def __str__(self):
+        return self.name
